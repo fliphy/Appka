54a5490622b3d8da8f02696f829992355803c9f5
Filip Ondrúšek <506454@muni.cz>
2021-03-01 21:50:23 +0100

    Test

--- Appka/Program.cs
+++ Appka/Program.cs
@@ -7,6 +7,8 @@ namespace Appka
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
